70feb9db145939d484f6461fb21ca08af4f66a07
Daniel C <brikbusters@gmail.com>
2025-04-29 22:39:57 -0400

    restore ui.c

--- test/ui.c
+++ test/ui.c
@@ -1,9 +1,35 @@
 #include <stdio.h>
 #include <rim.h>
 #include <im.h>
-#include <assert.h>
+
 int main(void) {
-	assert(0);
+	struct RimContext *ctx = rim_init();
+	int show_more = 1;
+	int counter = 0;
+	int selected = 0;
+	char buffer[64] = "asd";
+	while (rim_poll(ctx)) {
+		if (im_begin_window("My Window", 500, 500)) {
+			if (im_begin_tab_bar(&selected)) {
+				if (im_begin_tab("Tab thing")) {
+					im_entry("lbl", buffer, sizeof(buffer));
+					im_label(buffer);
+					if (im_button("Hello")) {
+						snprintf(buffer, sizeof(buffer), "changed %04d", counter++);
+					}
+					im_end_tab();
+				}
+				if (im_begin_tab("Tab #2")) {
+					if (im_button("Hello 2")) {
+						printf("Thing 2\n");
+					}
+					im_end_tab();
+				}
+				im_end_tab_bar();
+			}
+			im_end_window();
+		}
+	}
 
 	return 0;
 }
